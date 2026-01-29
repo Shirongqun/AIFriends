9a63a8623c0aca662626e5ff00b922dfe7fcbfe0
Shirongqun <19921273513@163.com>
2026-01-29 23:52:02 +0800

    实现数据库

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('usr',)  # 逗号千万不能删除！！！
 # Register your models here.
