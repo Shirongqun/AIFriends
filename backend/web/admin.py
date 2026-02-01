8b034bcecba10be04fd9779cbfaac6206b390fa6
Shirongqun <19921273513@163.com>
2026-02-01 22:24:32 +0800

    前端对接注册、登录、退出api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -3,5 +3,5 @@ from web.models.user import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    raw_id_fields = ('usr',)  # 逗号千万不能删除！！！
+    raw_id_fields = ('user',)  # 逗号千万不能删除！！！
 # Register your models here.
