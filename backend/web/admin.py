0f07612400e9851c2857b14ed470edf0e13cbe75
Shirongqun <19921273513@163.com>
2026-02-08 15:49:58 +0800

    实现角色增删改查后端api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from web.models.user import UserProfile
+from web.models.character import Character
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)  # 逗号千万不能删除！！！
 # Register your models here.
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
