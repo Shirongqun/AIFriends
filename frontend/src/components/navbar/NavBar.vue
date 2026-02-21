b922c578132f91cb30696b3c295c90e6c4493911
Shirongqun <19921273513@163.com>
2026-02-21 23:22:24 +0800

    打补丁

--- frontend/src/components/navbar/NavBar.vue
+++ frontend/src/components/navbar/NavBar.vue
@@ -33,7 +33,7 @@ const user = useUserStore()
           </div>
         </div>
         <div class="navbar-end">
-          <RouterLink v-if="user.isLogin()" active-class="btn-active" :to="{name: 'update-character', params: {character_id: 1}}" class="btn btn-ghost text-base mr-6">
+          <RouterLink v-if="user.isLogin()" active-class="btn-active" :to="{name: 'create-index'}" class="btn btn-ghost text-base mr-6">
             <CreateIcon />
             创作
           </RouterLink>
