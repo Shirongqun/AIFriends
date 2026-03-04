f8d87d024b0e1724bcbbd3387e17282a2d5982ce
Shirongqun <19921273513@163.com>
2026-03-04 21:31:11 +0800

    打补丁

--- frontend/src/views/create/character/components/BackgroundImage.vue
+++ frontend/src/views/create/character/components/BackgroundImage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onBeforeUnmount, ref, useTemplateRef, watch} from "vue";
+import {nextTick, onBeforeUnmount, ref, useTemplateRef, watch} from "vue";
 import CameraIcon from "@/views/user/profile/components/icons/CameraIcon.vue";
 import Croppie from "croppie";
 
@@ -17,6 +17,7 @@ let croppie = null
 
 async function openModal(photo) {
   modalRef.value.showModal()
+  await nextTick() // 等待页面渲染完，然后下边绑定图片
   if (!croppie) {
     croppie = new Croppie(croppieRef.value, {  // 创建croppie对象
       viewport: {width: 300, height: 500},
