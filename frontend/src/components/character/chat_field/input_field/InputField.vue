7d1840c75e0e93fcaefa03fc35b3b9e9c523ea58
Shirongqun <19921273513@163.com>
2026-03-15 21:48:06 +0800

    实现前端语音输入

--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -4,24 +4,31 @@ import SendIcon from "@/components/character/icons/SendIcon.vue";
 import MicIcon from "@/components/character/icons/MicIcon.vue";
 import {ref, useTemplateRef} from "vue";
 import streamApi from "@/js/http/streamApi.js";
+import Microphone from "@/components/character/chat_field/input_field/Microphone.vue";
 
 const props = defineProps(['friendId'])
 const emit = defineEmits(['pushBackMessage', 'addToLastMessage'])
 const inputRef = useTemplateRef('input-ref')
 const message = ref('')
-let isProcessing = false // 防止用户重复发消息
+let processId = 0  // 全局变量，可用于标识当前聊天窗口中新加入对话的版本号
+const showMic = ref(false)
 
 function focus() {
   inputRef.value.focus()
 }
 
-async function handleSend() {
-  if (isProcessing) return
-  isProcessing = true
-
+// 点击发送按钮后调用
+async function handleSend(event, audio_msg) {
   // 取出内容
-  const content = message.value.trim()
+  let content
+  if (audio_msg) {
+    content = audio_msg.trim()
+  } else {
+    content = message.value.trim()
+  }
   if (!content) return
+
+  const curId = ++ processId
   message.value = ''
 
   emit('pushBackMessage', {role: 'user', content: content, id: crypto.randomUUID()})
@@ -34,29 +41,39 @@ async function handleSend() {
         message: content
       },
       onmessage(data, isDone) {
-        if (isDone) {
-          isProcessing = false
-        } else if (data.content) {
+        if (curId !== processId) return
+
+        if (data.content) {
           emit('addToLastMessage', data.content)
         }
       },
       onerror(err) {
-        isProcessing = false
       },
     })
   } catch (err) {
-    isProcessing = false
   }
+}
+
+//
+function close() {
+  ++ processId  // 所有旧的对话都不再接受消息了
+  showMic.value = false
+}
+
+// 打断对方说话
+function handleStop() {
+  ++ processId
 }
 
 // 将接口暴漏给父组件
 defineExpose({
   focus,
+  close,
 })
 </script>
 
 <template>
-  <form @submit.prevent="handleSend" class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
+  <form v-if="!showMic" @submit.prevent="handleSend" class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
     <input
       ref="input-ref"
       v-model="message"
@@ -68,10 +85,17 @@ defineExpose({
     <div @click="handleSend" class="absolute right-2 w-8 h-8 flex justify-center items-center cursor-pointer">
       <SendIcon />
     </div>
-    <div class="absolute right-10 w-8 h-8 flex justify-center items-center cursor-pointer">
+    <div @click="showMic = true" class="absolute right-10 w-8 h-8 flex justify-center items-center cursor-pointer">
       <MicIcon />
     </div>
   </form>
+<!--  麦克风组件-->
+  <Microphone
+      v-else
+      @close="showMic = false"
+      @send="handleSend"
+      @stop="handleStop"
+  />
 </template>
 
 <style scoped>
